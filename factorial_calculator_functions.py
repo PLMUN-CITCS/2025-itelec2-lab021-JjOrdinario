46170aeeb88568aaad188621fddd4c5d912e4989
JjOrdinario <johnjenricordi@gmail.com>
2025-03-11 23:58:31 +0800

    Submitting Python Week 05 - Laboratory # 21

--- factorial_calculator_functions.py
+++ factorial_calculator_functions.py
@@ -1,18 +1,10 @@
-# Function to calculate factorial
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        result = 1
-        for i in range(1, n + 1):
-            result *= i
-        return result
+n = int(input("Enter a non-negative integer: "))
 
-# Input from user
-num = int(input("Enter a non-negative integer: "))
-
-# Check if the number is valid
-if num < 0:
-    print("Please enter a non-negative integer.")
+if n < 0:
+    print("Please enter a non-negative integer.")  
 else:
-    print(f"The factorial of {num} is: {factorial(num)}")
+    factorial = 1
+    for i in range(1, n + 1):  
+        factorial *= i
+
+    print(f"The factorial of {n} is: {factorial}")
